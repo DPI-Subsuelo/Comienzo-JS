b6dad38932c3f2108c38622b85aa08f7fdb4f2ac
Agustina <agustina.frias12@gmail.com>
2025-01-10 12:07:24 -0300

    first commit

---
+++ archivos.js
@@ -0,0 +1,10 @@
+//concatenar y hacer comentarios
+let _nombre= "Rita";
+let _apellido= "Castro";
+document.write(_nombre +" "+_apellido);
+//concatenar con coma
+document.write("<br>");
+document.write(_nombre," ",_apellido);
+/*Para comentar un codigo
+de varias linesas se utiliza "+" y "/" 
+en principio y fin*/
---
+++ variables.js
@@ -0,0 +1,16 @@
+//var nombre= " Ana";
+//var apellido= " Diaz";
+//var CUIT= "";
+
+
+let saludo= "Hola";
+let Saludo= "Buenos días";
+let saludo1= "¿Cómo estas?";
+let saludo2= "Bienvenido";
+let persona= {Nombre: "Agustina" , edad: 27}
+
+document.write (saludo2);
+document.write (nombre);
+document.write (apellido);
+document.write ("<br>");
+document.write (persona.edad)
