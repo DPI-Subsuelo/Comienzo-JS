ddeb57c66ed82562c8881854bfb724f70f1e0193
Agustina <agustina.frias12@gmail.com>
2025-01-13 11:53:59 -0300

    op.asignación

---
+++ arch.js
@@ -0,0 +1,21 @@
+//ver la estructura de los operadores de asignación, incremento y decremento
+let a = 6;
+document.write("valor de a: ", a);
+document.write("<br>");
+a++; //aumenta un valor
+document.write("valor de a: ", a);
+document.write("<br>");
+a+=3;//aumento de variable
+document.write("valor de a: ",a);
+document.write("<br>");
+a--;
+document.write("valor de a: ", a);
+document.write("<br>");
+a-=5;
+document.write("valor de a: ", a);
+document.write("<br>");
+a/=2;
+document.write("valor de a: ",a);
+document.write("<br>");
+a**=3;
+document.write("valor: ",a);
