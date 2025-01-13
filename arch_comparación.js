e163751c0753477c8fcaa3139c9115838d103132
Agustina <agustina.frias12@gmail.com>
2025-01-13 12:36:44 -0300

    op.coparaciona

---
+++ arch_comparación.js
@@ -0,0 +1,17 @@
+let = valor1, valor2;
+valor1= 20;
+valor2= 10;
+
+document.write(valor1 > valor2); //si el 1 es mayor a al2
+//document.write("<br>");
+//document.write(valor1 < valor2); // si el valor1 es menor al 2
+//document.write("<br>");
+//document.write(valor1 == valor2) //compara si son iguales
+//document.write("<br>");
+//document.write(valor1 != valor2); //pregunto si el valor1 es diferente al 2
+//document.write("<br>");
+//document.write(valor1 <= valor2); //si el valor1 es menor o igual al 2
+//document.write("<br>");
+//document.write(valor1 >= valor2);// si el valor 1 es mayor o igual al 2
+//document.write("<br>");
+//document.write(valor1 = valor);
