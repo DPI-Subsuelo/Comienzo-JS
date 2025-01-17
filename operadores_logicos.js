ca6f356f4c93901d91b998d5c4aa9e240a2984ca
Agustina <agustina.frias12@gmail.com>
2025-01-17 12:06:22 -0300

    operadores logicos

---
+++ operadores_logicos.js
@@ -0,0 +1,18 @@
+//Operador && (y)
+// let pais, edad
+// pais= prompt('Ingrese su País');
+// edad= prompt('Ingrese su edad');
+
+// if(pais=='Argentina' && edad >= 18){
+//     document.write('Eres un argentino, mayor de edad');
+// }else {document.write('No cumples con los requisitos establecidos')}
+
+//funcion de las || , funciona como un "o"
+let dia, mes, anio;
+dia= prompt('Ingrese día: ');
+mes= prompt('Ingrese mes: ');
+anio= prompt('Ingrese año: ');
+
+if (mes== 1 || mes== 2 || mes==3){document.write('Pertenece al 1er trimestre');
+}
+else{document.write('No pertenece al 1er trimestre')};
