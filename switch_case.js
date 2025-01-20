8f14e6c04418da885613cddf6a134a079ac23f7a
Agustina <agustina.frias12@gmail.com>
2025-01-20 12:48:14 -0300

    Condicionales

---
+++ switch_case.js
@@ -0,0 +1,35 @@
+/*let valor
+valor= parseInt (prompt("Ingrese un valor del 1 al 3"));
+
+switch (valor) {
+    case 1:
+        document.write('Ingreso el uno');
+        break;
+    case 2:
+        document.write('Ingreso el dos');
+        break;
+    case 3:
+        document.write('Ingreso el tres');
+        break;
+
+    default:// controla todo lo que pasa si no sse cumple el case 1,2 o 3;
+     document.write('No es un valor válido')
+
+        break;
+}*/
+let color
+color= prompt('Ingrese un color: rojo/verde/azul, en minusculas' );
+switch (color) {
+    case 'rojo':
+        document.write('Ingreso rojo');
+        break;
+     case 'verde':
+        document.write('Ingreso verde');
+        break;
+    case 'azul':
+        document.write('Ingreso azul');
+        break;
+    default:
+        document.write('No es valido el color');
+        break;
+}
---
+++ while_1.js
@@ -0,0 +1,28 @@
+//while: es una estructura de control de bucle, va realizando procesos uno detras de oto mientra una condicion que se especifica se cumple, si esto no se cumple el bicle deja de ejecutarse, ye codigo continua su curso.
+/*let f =1;
+
+while (f<=10) {
+    document.write('vuelta numero: ', f);
+    document.write('<br>');
+    f++;
+}
+document.write('FIN DEL BUCLE!!')*/
+
+/*let p =10
+while (p>0) {
+    document.write('vuelta número: ', p);
+    document.write('<br>');
+    p--;
+
+}
+document.write('Fin del bucle!!')*/
+
+let a= 1;
+let suma=0; 
+let valor;
+while (x <= 5) {
+    valor= parseInt (prompt('Ingrese valor'));
+    suma= suma + valor;
+    f++;
+}
+document.write('La suma es: ', suma, '<br>');
