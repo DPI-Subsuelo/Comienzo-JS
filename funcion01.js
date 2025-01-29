d174167ed67a486a3e2920440a8a5a8464649a7b
Agustina <agustina.frias12@gmail.com>
2025-01-29 12:19:00 -0300

    comienzo de funciones

---
+++ funcion01.js
@@ -0,0 +1,19 @@
+//funcion declarativa, dentro del parentesis se coloca un parámetro, pero al ser declarativa se trabaja vacio
+function saludo () {
+    document.write("Bienvenidos a la página!!")
+//alert("Bienvenidos a la página!!")
+}
+//las funciones para que se muestren, deben ser invocadas 
+
+function despedida () {
+    document.write("Gracias por tu visita!")
+
+}
+
+saludo();
+document.write("<br>");
+document.write("<br>");
+document.write("<br>");
+document.write("<br>");
+despedida();
+
