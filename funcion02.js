cde5b775b47d1c483227a91a9027ec067f1ab7df
Agustina <agustina.frias12@gmail.com>
2025-01-30 12:57:24 -0300

    funcion con parametro

---
+++ funcion02.js
@@ -0,0 +1,20 @@
+/*let resultado;
+function suma(valor1, valor2) {
+valor1 = parseInt (valor1);
+valor2 = parseInt (valor2);
+    resultado= valor1 + valor2;
+    alert ("Resultado: "  + resultado);
+
+    
+}
+suma(8,5);*/
+
+let resultado;
+function suma(valorA, valorB) {
+    valorA= parseInt (prompt ("Ingrese valor 1"));
+    valorB= parseInt (prompt("Ingrese valor 2"));
+    resultado = valorA + valorB;
+    alert ("Resultado: "+ resultado);
+
+}
+suma();
