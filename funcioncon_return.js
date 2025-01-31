38f35e663d13a4a659a9c43fe579c0a6f6c1aab7
Agustina <agustina.frias12@gmail.com>
2025-01-31 12:36:09 -0300

    agregado

--- funcioncon_return.js
+++ funcioncon_return.js
@@ -1,11 +1,32 @@
+let resultado
 function suma (a , b){
     resultado = a + b 
     return resultado
-
 }
 document.write(suma(30 , 60));
 document.write("<br>");
 
+//---------------------------
 function saludar (){
-    
+    return "Hola a todos!!";
+
 }
+var mensaje = saludar();
+document.write(mensaje);
+//--------------------------
+function vercolor (valor) {
+    valor= parseInt(prompt("Ingrese un valor: 1 / 3"));
+    switch (valor){
+        case 1: 
+            return "Rojo";
+        case 2:
+            return "Verde";
+        case 3:
+            return "Amarillo";
+        default:
+            return "valor incorrecto";
+    }
+}
+//alert(vercolor ());
+document.write("<br>");
+document.write(vercolor());
