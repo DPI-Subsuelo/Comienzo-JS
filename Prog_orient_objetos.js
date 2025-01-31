1bf85bd23ec3177d5e0f92de776eea1bea4e4830
Agustina <agustina.frias12@gmail.com>
2025-01-31 12:36:18 -0300

    POO

---
+++ Prog_orient_objetos.js
@@ -0,0 +1,4 @@
+//esta programacion los progarmas se diseñan y organizan en torno a objetos: estructura que contine propiedades y metodos (funciones),
+//atribuye propiedades como herencia, abstracion, encapsulamiento, polimorfismo  
+//En js esta programacion se separa en clases( entesd abstractos que definen un conjunto de objetos; 
+// es decir, los objetos son miembros de las clases) e instancias( son los mismos objetos)
