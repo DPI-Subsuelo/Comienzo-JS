aa820646f1f5bc685a06d56a827091c89d98bff8
Agustina <agustina.frias12@gmail.com>
2025-02-04 11:57:03 -0300

    atributos y metodos POO

---
+++ orient_objetos02.js
@@ -0,0 +1,29 @@
+class Persona {
+    nombre ="Homero "; //atributos
+    apellido= "Simpson";
+    domicilio= "Av. Siempreviva ";
+    telefono="123456789";
+    email= "homero.simpson@gmail.com";
+
+    trabaja(){ //metodos
+        return "Trabaja en la planta nuclear";
+    }
+    Estudia(){
+        return "Escuela Primaria de Sprinfield";
+
+    }
+
+}
+const homero= new Persona();
+const Bart= new Persona();
+const Lenny= new Persona();
+document.write (homero.nombre +" "+ homero.apellido);
+document.write("<br>");
+document.write (homero.trabaja ());
+document.write("<br>");
+document.write("<br>");
+document.write("Bart ", Bart.apellido);
+document.write("<br>");
+document.write("Estudia en: ", Bart.Estudia());
+document.write("<br>");
+document.write("Lenny ", Lenny.trabaja())
