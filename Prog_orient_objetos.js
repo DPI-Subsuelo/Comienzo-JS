dcaf8b5591a1e4f03ed470e48c0f3f92ac4c3204
Agustina <agustina.frias12@gmail.com>
2025-02-04 11:56:44 -0300

    intro Poo

--- Prog_orient_objetos.js
+++ Prog_orient_objetos.js
@@ -2,3 +2,16 @@
 //atribuye propiedades como herencia, abstracion, encapsulamiento, polimorfismo  
 //En js esta programacion se separa en clases( entesd abstractos que definen un conjunto de objetos; 
 // es decir, los objetos son miembros de las clases) e instancias( son los mismos objetos)
+var Auto = {
+    marca: "Toyota",
+    modelo: "corolla" , //llevan comillas y una coma , no punto y coma
+    anio: "2022" ,
+    color: "Negro"
+}
+document.write("Creación del objeto: Auto");
+document.write("<br>");
+document.write("marca: ", Auto.marca);
+document.write("<br>");
+document.write("Modelo: ", Auto.modelo);
+document.write("<br>");
+document.write("Año: ", Auto.anio);
