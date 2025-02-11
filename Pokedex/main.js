f6894a71ca45357371d3815fc3d92cf437fbad7d
Agustina <agustina.frias12@gmail.com>
2025-02-11 11:55:17 -0300

    pokedex,terminado falla

--- Pokedex/main.js
+++ Pokedex/main.js
@@ -3,14 +3,31 @@ const URL="https://pokeapi.co/api/v2/pokemon"
 const searchInput =document.getElementById("search");
 const pokedexContainer = document.getElementById("pokedex");
 
+//async se utilizo para manejar la peticion a la APi de forma asincronica. Mientras se espera
+//la resp de la Api, el programa puede seguir ejeutando otras tareas sin bloquearse
 async function searchPokemon() {
-    const searchPokemon= searchInput.ariaValueMax.toLowerCase();
+    const searchPokemon= searchInput.value.toLowerCase();
 
     try{
-        const response= await fetch (URL + searchPokemon)
+        const response= await fetch (`${URL}/${searchPokemon}`); // Corregida la URL
+        if (!response.ok) {
+            throw new Error("Pokémon no encontrado");
+        }
         const data = await response.json();
 
+        pokedexContainer.innerHTML= `
+        
+            <h2>${data.name.toLowerCase()}</h2>
+            <img src = "${data.sprites.front_default}" alt="${data.name}"></img>
+            <p>Numero: ${data.id}</p>
+            <p>Altura: ${data.height/10} m</p>
+            <p>Peso: ${data.weight/10} kg </p>
+            `;
+
     } catch(error) {
+        console.error(error);
+        pokedexContainer.innerHTML = `<p style="color: red;">Pokémon no encontrado</p>`
 
     }
 }
+document.getElementById("btn-search").addEventListener("click", searchPokemon);
